1221ad9d820dd3506cf9a83431d118390e7e0f8d
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-08-01 01:08:09 -0700

    Create bottom tab bar

--- Frontend/iOSApp/iOS-Flashcards-App/iOS-Flashcards-App/Screens/Home/HomeView.swift
+++ Frontend/iOSApp/iOS-Flashcards-App/iOS-Flashcards-App/Screens/Home/HomeView.swift
@@ -11,18 +11,34 @@ struct HomeView: View {
     @EnvironmentObject private var user: User
     @StateObject private var viewModel = ViewModel()
     var body: some View {
-        VStack {
-            Text("Currently logged in")
-                .font(.system(size: 26, weight: .bold))
-            Button(action: {
-                viewModel.logout()
-            }) {
-                Text("Click here to logout")
+        TabView {
+            VStack {
+                Text("Currently logged in")
+                    .font(.system(size: 26, weight: .bold))
+                Button(action: {
+                    viewModel.logout()
+                }) {
+                    Text("Click here to logout")
+                }
+                Spacer()
+            }.onAppear {
+                // Pass environment object into viewmodel
+                viewModel.initUser(user: user)
             }
-            Spacer()
-        }.onAppear {
-            // Pass environment object into viewmodel
-            viewModel.initUser(user: user)
+            .tabItem {
+                Image(systemName: "house")
+                Text("Home")
+            }
+            SearchView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search")
+                }
+            Text("Profile") // Create user settings view later
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("My Profile")
+                }
         }
     }
 }
