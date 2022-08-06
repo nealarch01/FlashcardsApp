fb1c0d554aa11eca3a99879f2b9a91e0880d80da
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-08-06 00:41:28 -0700

    Create init for color that accepts Hex

--- Frontend/iOSApp/iOS-Flashcards-App/iOS-Flashcards-App/AppColors.swift
+++ Frontend/iOSApp/iOS-Flashcards-App/iOS-Flashcards-App/AppColors.swift
@@ -14,4 +14,13 @@ extension Color {
     public static let babyBlue = Color("babyBlue")
     public static let skyBlue = Color("SkyBlueCrayola")
     public static let appWhite = Color("AppWhite")
+    
+    // Custom initializer than accepts a hexadecimal
+    init(hex: Int, opacity: Double = 1.0) {
+        // Note: 0xff = 255
+        let red = Double((hex & 0xff000) >> 16) / 255.0
+        let green = Double((hex & 0xff00) >> 4) / 255.0
+        let blue = Double((hex & 0xff) >> 0) / 255.0
+        self.init(.sRGB, red: red, green: green, blue: blue)
+    }
 }
