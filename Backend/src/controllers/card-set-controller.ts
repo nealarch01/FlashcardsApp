f312a37efe8a84bdb86fe990e580ebbd063c2094
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-08-08 00:45:40 -0700

    Create card set controller and model

--- Backend/src/controllers/card-set-controller.ts
+++ Backend/src/controllers/card-set-controller.ts
@@ -3,3 +3,86 @@ import { Request, Response } from "express";
 import AuthenticationToken from "../middlewares/authentication-token";
 import verifyRequestBody from "../middlewares/verify-request-body";
 
+import { GenericTypes as types } from "../utils/types";
+
+import CardSetModel from "../models/card-set-model";
+
+class CardSetController {
+    // POST
+    async createCardSet(req: Request, res: Response) {
+        const authToken = req.headers.authorization;
+        if (authToken === undefined) {
+            return res.status(401).send({
+                message: "Authorization was not provided"
+            });
+        }
+        const reqBody = JSON.parse(req.body);
+        const reqBodyErr = verifyRequestBody(reqBody, ["title", "description"], [types.string, types.string]);
+        if (reqBodyErr !== null) {
+            return res.status(400).send({
+                message: "Invalid title or description format."
+            });
+        }
+        const user_id = AuthenticationToken.decode(authToken);
+        const title = reqBody.title;
+        const description = reqBody.description;
+
+        let newCardSetID = await CardSetModel.createCardSet(user_id!, title, description);
+        return res.status(201).send({
+            new_resource_id: newCardSetID,
+            message: "Card set created successfully.",
+        });
+    }
+
+    async deleteCardSet(req: Request, res: Response) {
+        const authToken = req.headers.authorization || "";
+        if (!AuthenticationToken.isValid(authToken)) {
+            return res.status(400).send({
+                message: "Invalid authorization token."
+            });
+        }
+
+        const setID = parseInt(req.params.set_id);
+        if (isNaN(setID)) {
+            return res.status(400).send({
+                message: "Invalid setID format."
+            });
+        }
+
+        // Check if the user owns the set
+        const user_id = AuthenticationToken.decode(authToken);
+        const ownedSets = await CardSetModel.getCardSetsFromCreator(user_id!);
+        if (!ownedSets.some(set => set.id === setID)) {
+            return res.status(400).send({
+                message: "You do not own this set."
+            });
+        }
+
+        let deleteStatus = await CardSetModel.deleteCardSet(user_id!, setID);
+
+        if (deleteStatus === true) {
+            return res.status(200).send({
+                message: "Card set deleted successfully."
+            });
+        }
+        return res.status(500).send({
+            message: "Card set could not be deleted."
+        });
+    }
+
+
+    // GET 
+    async getCardsInSet(req: Request, res: Response) {
+        const setID = parseInt(req.params.set_id);
+        if (isNaN(setID)) {
+            return res.status(400).send({
+                message: "Invalid set_id format."
+            });
+        }
+        const cards = await CardSetModel.getCardsInSet(setID);
+    }
+
+    // GET 
+}
+
+export default new CardSetController();
--- Backend/src/models/card-set-model.ts
+++ Backend/src/models/card-set-model.ts
@@ -1,8 +1,100 @@
 import Database from "./database";
 
+import { CardSetMetaData, CardData } from "../utils/types";
+
 class CardSetModel {
-    // Get user_id
-    async createCardSet(creator: number) {
-        let queryString = `INSERT INTO card_set (user_id, )`
+    // Return the insert id of the newly created card set
+    async createCardSet(creator_id: number, title: string, description: string): Promise<number> {
+        let queryString = `INSERT INTO card_set (creator_id, title, description, created_at) VALUES (?, ?, ?, ?, NOW());`;
+        let values: Array<any> = [creator_id, title, description];
+        let queryResult: any = await Database.safeQuery(queryString, values);
+        return queryResult.insertId;
+    }
+
+    async getCardsInSet(set_id: number): Promise<Array<CardData>> {
+        let queryString = `SELECT * FROM card WHERE id IN (SELECT card_id from set_data WHERE set_id=${set_id});`;
+        let queryResult: any = await Database.query(queryString);
+        let cards: Array<CardData> = [];
+        for (let i = 0; i < queryResult.length; i++) {
+            let cardData: CardData = {
+                id: queryResult[i].id,
+                presented: queryResult[i].presented,
+                hidden: queryResult[i].hidden
+            };
+        }
+        return cards;
+    }
+
+    async getCardSetMetaDataFromID(card_set_id: number): Promise<CardSetMetaData | undefined> {
+        let queryString = `SELECT * FROM card_set WHERE id=${card_set_id};`;
+        let queryResult: any = await Database.query(queryString);
+        if (queryResult.length === 0) {
+            return undefined;
+        }
+        let cardSetMetadata: CardSetMetaData = {
+            id: queryResult[0].id,
+            creator_id: queryResult[0].creator_id,
+            title: queryResult[0].title,
+            description: queryResult[0].description,
+            created_at: queryResult[0].created_at,
+            private: queryResult[0].private === 1 ? true : false
+        };
+        return cardSetMetadata;
+    }
+
+    async getCardSetsFromCreator(creator_id: number): Promise<Array<CardSetMetaData>> {
+        let queryString = `SELECT * FROM card_set WHERE creator_id=${creator_id};`;
+        let queryResult: any = await Database.query(queryString);
+        let cardSetMetadata: Array<CardSetMetaData> = [];
+        for (let i = 0; i < queryResult.length; i++) {
+            let cardSet: CardSetMetaData = {
+                id: queryResult[i].id,
+                creator_id: creator_id,
+                title: queryResult[i].title,
+                description: queryResult[i].description,
+                created_at: queryResult[i].created_at,
+                private: queryResult[i].private === 1 ? true : false
+            };
+            cardSetMetadata.push(cardSet);
+        }
+        return cardSetMetadata;
+    }
+
+    async searchCardSetFromTitle(title: string): Promise<CardSetMetaData | undefined> {
+        let queryString = `SELECT * FROM card_set WHERE title LIKE ?;`;
+        let values: Array<string> = [title];
+        let queryResult: any = await Database.safeQuery(queryString, values);
+        if (queryResult.length === 0) {
+            return undefined;
+        }
+        let cardSetMetadata: CardSetMetaData = {
+            id: queryResult[0].id,
+            creator_id: queryResult[0].creator_id,
+            title: queryResult[0].title,
+            description: queryResult[0].description,
+            created_at: queryResult[0].created_at,
+            private: queryResult[0].private === 1 ? true : false
+        };
+        return cardSetMetadata;
+    }
+
+    async transferCardSetOwnership(creator_id: number, card_set_id: number, new_creator_id: number): Promise<boolean> {
+        let queryString = `UPDATE card_set SET creator_id=${new_creator_id} WHERE id=${card_set_id};`;
+        let queryResult: any = await Database.query(queryString);
+        if (queryResult.affectedRows === 0) {
+            return false;
+        }
+        return true;
+    }
+
+    async deleteCardSet(creator_id: number, card_set_id: number): Promise<boolean> {
+        let queryString = `DELETE FROM card_set WHERE creator_id=${creator_id} AND id=${card_set_id};`;
+        let queryResult: any = await Database.query(queryString);
+        if (queryResult.affectedRows === 0) {
+            return false;
+        }
+        return true;
     }
 }
+
+export default new CardSetModel();
