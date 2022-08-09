fc5c380ce962dcc7b4ae1cdaa2581f8a0407c0b8
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-08-09 11:29:15 -0700

    Create card-set Router

---
+++ Backend/src/routes/card-set-router.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+
+    import CardSetController from "../controllers/card-set-controller";
+
+class CardSetRouter {
+    // GET 
+    getCardsInSetRouter = Router().get("/card-set/:setID", CardSetController.getCardsInSet);
+
+    // GET
+    getCardSetsFromCreatorRouter = Router().get("/card-set/:user_id", CardSetController.getCardSetsFromCreator);
+
+    // POST 
+    createCardSetRouter = Router().post("/card-set/create", CardSetController.createCardSet)
+
+    // DELETE
+    deleteCardSetRouter = Router().delete("/card-set/delete/:setID", CardSetController.deleteCardSet)
+
+    // PUT
+    updateTitleRouter = Router().put("/card-set/update-title/", CardSetController.updateCardSetTitle)
+
+    // PUT 
+    updateDescriptionRouter = Router().put("/card-set/update-description/", CardSetController.updateCardSetDescription) 
+
+    // GET
+    getCardSetsFromCreator = Router().get("/card-set/:user_id", CardSetController.getCardSetsFromCreator)
+
+    // GET
+    getCardSetDataFromID = Router().get("/card-set/:setID", CardSetController.getCardSetMetaDataFromID)
+
+    // GET
+    getCardsInSet = Router().get("/card-set/:setID", CardSetController.getCardsInSet)
+}
+
+export default new CardSetRouter();
