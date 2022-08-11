42899c1a88573a856a0e9463d9ad7c246f1ee14b
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-08-10 17:38:14 -0700

    Implement card flip with tap gesture

--- Frontend/iOSApp/iOS-Flashcards-App/iOS-Flashcards-App/Screens/Cards/FlashcardView.swift
+++ Frontend/iOSApp/iOS-Flashcards-App/iOS-Flashcards-App/Screens/Cards/FlashcardView.swift
@@ -12,13 +12,12 @@ struct FlashcardView: View {
     
     private let shortString: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     private let longString: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+    
     @State private var isPresentedShown: Bool = true // True by default
     
     @State private var translation: CGSize = .zero
     @State private var cardRotation: Double = 0.0
     @State private var textRotation: Double = 0.0 // This is necessary. Without this, the text will be shown backwards
-    
-    private let slidePercentage: Double = 0.3 // 1/2 of the screen's width
     
     var body: some View {
         GeometryReader { geometry in
@@ -51,14 +50,16 @@ struct FlashcardView: View {
                 .rotation3DEffect(.degrees(cardRotation), axis: (x: 0, y: 1, z: 0))
                 .overlay {
                     RoundedRectangle(cornerRadius: 12)
-                        .stroke(Color.black, lineWidth: 3)
+                        .stroke(Color.black.opacity(0.9), lineWidth: 2)
                         .rotation3DEffect(.degrees(cardRotation), axis: (x: 0, y: 1, z: 0))
                 }
-                
+                .onTapGesture {
+                    flipCard(geometry: geometry, translationWidth: geometry.size.width * -0.51)
+                }
                 .gesture (
                     DragGesture()
                         .onEnded { value in
-                            flipCard(geometry: geometry, gesture: value)
+                            flipCard(geometry: geometry, translationWidth: value.translation.width)
                         }
                 )
             }
@@ -68,29 +69,32 @@ struct FlashcardView: View {
     }
     
     // Right swipe (finger starts from the right and ends at the left)
-    private func flipCard(geometry: GeometryProxy, gesture: DragGesture.Value) -> Void {
-        let dragPercentage = gesture.translation.width / geometry.size.width
-        let flipDuration: Double = 0.21
+    private func flipCard(geometry: GeometryProxy, translationWidth: CGFloat) -> Void {
+        let dragPercentage = translationWidth / geometry.size.width
+        let flipDuration: Double = 0.3
+        let slidePercentage: Double = 0.3 // 3/10 of the screen's width
+        
+        let flipAnimation = Animation.spring()
         
         var rotationForText: Double
         
         if dragPercentage >= slidePercentage { // Finger starts from left, and ends right
             // Rotate from right -> left
             rotationForText = 180
-            withAnimation(.easeOut(duration: flipDuration)) {
+            withAnimation(flipAnimation) {
                 self.cardRotation += 180
             }
         } else if dragPercentage <= -slidePercentage { // Finger starts from right, and ends left
             // Rotate from left -> right
             rotationForText = -180
-            withAnimation(.easeOut(duration: flipDuration)) {
+            withAnimation(flipAnimation) {
                 self.cardRotation -= 180
             }
         } else {
             return
         }
         
-        withAnimation(.easeOut(duration: 0).delay(flipDuration / 2.0)) { // Wait until half way flipped then change the text
+        withAnimation(.linear(duration: 0).delay(flipDuration / 2.0)) { // Wait until half way flipped then change the text
             self.textRotation += rotationForText
             self.isPresentedShown.toggle()
         }
