beb4687063e0f7850aa5f981c2b7486e1d2161e4
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-08-19 10:02:02 -0700

    Create check owernship method

--- Backend/src/models/card-model.ts
+++ Backend/src/models/card-model.ts
@@ -2,6 +2,7 @@ import { CardData } from "../utils/types";
 import Database from "./database";
 
 class CardModel {
+    // Creates a new card and returns its insert ID.
     async createCard(presented: string, hidden: string): Promise<number> {
         let queryString = `INSERT INTO card (presented, hidden) VALUES (?, ?);`;
         let values: Array<string> = [presented, hidden];
@@ -10,6 +11,9 @@ class CardModel {
         return queryResult.insertId;
     }
 
+
+
+    // Edits the presented text of card of the given cardID.
     async editPresented(id: number, newPresented: string): Promise<boolean> {
         let queryString = `UPDATE card SET presented=? WHERE id=${id};`;
         let values: Array<string> = [newPresented];
@@ -20,6 +24,7 @@ class CardModel {
         return true;
     }
 
+    // Edits the hidden text of a card of the given cardID
     async editHidden(id: number, newHidden: string): Promise<boolean> {
         let queryString = `UPDATE card SET hidden=? WHERE id=${id};`;
         let values: Array<string> = [newHidden];
@@ -30,6 +35,9 @@ class CardModel {
         return true;
     }
 
+
+
+    // Gets a card of a certain cardID.
     async getCard(card_id: number): Promise<CardData | undefined> {
         let queryString = `SELECT * FROM card WHERE id=${card_id};`;
         let queryResult: any = await Database.query(queryString);
@@ -44,10 +52,24 @@ class CardModel {
         return cardData;
     }
 
+
+
+    // Deletes a card of the given cardID.
     async deleteCard(card_id: number): Promise<boolean> {
         let queryString = `DELETE FROM card WHERE id=${card_id};`;
         let queryResult: any = await Database.query(queryString);
         if (queryResult.affectedRows === 0) {
+            return false;
+        }
+        return true;
+    }
+
+
+    // Function checks if the user owns the card. 
+    async checkCardOwnership(card_id: number, user_id: number): Promise<boolean> {
+        let queryString = `SELECT * FROM card WHERE id=${card_id} AND user_id=${user_id};`;
+        let queryResult: any = await Database.query(queryString);
+        if (queryResult.length === 0) {
             return false;
         }
         return true;
