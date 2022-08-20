0bcba7f970817121c1bd31abb84bd0ba48b14a39
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-08-20 11:39:16 -0700

    Create card router

---
+++ Backend/src/routes/card-router.ts
@@ -0,0 +1,20 @@
+import CardController from "../controllers/card-controller";
+
+import { Router } from "express";
+
+class CardRouter {
+    // POST ; Creates a new flashcard (does not assign to a flashcard set).
+    createCard = Router().post("/create", CardController.createCard); 
+
+    // DELETE ; Deletes a flashcard.
+    deleteCard = Router().delete("/delete/:cardID", CardController.deleteCard); 
+
+    // PUT ; Updates the presented text of a flashcard.
+    updatePresentedText = Router().put("/update-presented-text/:cardID", CardController.updatePresentedText);
+
+    // PUT ; Updates the hidden text of a flashcard.
+    updateHiddenText = Router().put("/update-hidden-text/:cardID", CardController.updateHiddenText);
+
+    // GET ; Gets the flashcard with the given cardID.
+    getCard = Router().get("/:cardID", CardController.getCard);
+}
