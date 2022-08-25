2bd0d5c95eb909679e4cf7bc04211bb3659ab65a
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-08-25 13:58:20 -0700

    Create API fetch method
    Create new method to fetch cards in a set. Remove default init constructor, initialize Flashcard Set inline

--- Frontend/iOSApp/iOS-Flashcards-App/iOS-Flashcards-App/Screens/Cards/ViewModels/FlashcardSetViewModel.swift
+++ Frontend/iOSApp/iOS-Flashcards-App/iOS-Flashcards-App/Screens/Cards/ViewModels/FlashcardSetViewModel.swift
@@ -9,10 +9,18 @@ import Foundation
 
 extension FlashcardSetView {
     @MainActor final class ViewModel: ObservableObject {
-        @Published var flashcardSet: FlashcardSet
+        @Published var flashcardSet: FlashcardSet = FlashcardSet() // Default constructor
+        @Published var errorMessage: String = ""
         
-        init() {
-            flashcardSet = FlashcardSet()
+        public func fetchCards(authToken: String) async -> Array<Flashcard> {
+            let setID = self.flashcardSet.id
+            let (cards, err) = await FlashcardService().fetchCardsInSet(setID: setID, authToken: authToken)
+            if err != nil {
+                errorMessage = err!
+                print(errorMessage)
+                return []
+            }
+            return cards
         }
     }
 }
