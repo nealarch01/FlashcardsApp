2d4684c9b9188c216e162b19685cdfe0fa726486
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-08-25 13:49:25 -0700

    Create view model for flashcard set list view

---
+++ Frontend/iOSApp/iOS-Flashcards-App/iOS-Flashcards-App/Screens/Cards/ViewModels/FlashcardSetListViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  FlashcardSetListViewModel.swift
+//  iOS-Flashcards-App
+//
+//  Created by Neal Archival on 8/24/22.
+//
+
+import Foundation
+
+extension FlashcardSetListView {
+    @MainActor final class ViewModel: ObservableObject {
+        @Published var errorMessage: String = ""
+        
+        public func fetchFlashcardSets(fetchType: FlashcardSetList.SetType, authToken: String) async -> Array<FlashcardSet> {
+            var sets: Array<FlashcardSet>
+            var err: String?
+            switch fetchType {
+            case .owned:
+                (sets, err) = await FlashcardService().fetchOwnedSets(authToken: authToken)
+                break
+            }
+            
+            if err != nil {
+                errorMessage = err!
+                return []
+            }
+            
+            return sets
+        }
+        
+    }
+}
