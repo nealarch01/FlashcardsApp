2f538cd24e80c290f2dfe970e254926af6472d48
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-08-25 13:51:20 -0700

    Implement API connectivity
    View and ViewModel obtains owned sets from Web API

--- Frontend/iOSApp/iOS-Flashcards-App/iOS-Flashcards-App/Screens/Cards/Views/FlashcardSetListView.swift
+++ Frontend/iOSApp/iOS-Flashcards-App/iOS-Flashcards-App/Screens/Cards/Views/FlashcardSetListView.swift
@@ -7,20 +7,67 @@
 
 import SwiftUI
 
+
+
 struct FlashcardSetListView: View {
+    @StateObject private var viewModel = ViewModel()
+    
     @State var flashcardSets: Array<FlashcardSet>
+    
+    let fetchType: FlashcardSetList.SetType
+    
     let title: String
+    
+    @State private var isLoading: Bool = false
+    
+    
+    @EnvironmentObject private var user: User
+    
     var body: some View {
-        List {
-            NavigationLink(destination: FlashcardSetView(flashcardSet: FlashcardSet())) {
-                Text(flashcardSets[0].title)
+        ZStack {
+            if flashcardSets.count == 0 {
+                VStack {
+                    Text("No flashcard sets.")
+                        .font(.system(size: 24, weight: .medium))
+                        .padding([.top], 15)
+                    Spacer()
+                }
+            } else {
+                List {
+                    ForEach(flashcardSets, id: \.self.id) { flashcardSet in
+                        NavigationLink(destination: FlashcardSetView(flashcardSet: flashcardSet)) {
+                            Text(flashcardSet.title)
+                        }
+                    }
+                } // End of list
             }
-        }.navigationTitle(title)
+        } // End of ZStack
+        .navigationTitle(title)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {}) {
+                    Text("New")
+                }
+            } // End of ToolbarItem
+        } // End of toolbar
+        .onAppear {
+            Task {
+                let response = await viewModel.fetchFlashcardSets(fetchType: .owned, authToken: self.user.authToken)
+                flashcardSets = response // Note, this does not obtain flashcard sets
+            }
+        }
+    }
+    
+    private func deleteFlashcardSet() {
+        return
     }
 }
 
 struct FlashcardSetListView_Previews: PreviewProvider {
     static var previews: some View {
-        FlashcardSetListView(flashcardSets: [FlashcardSet()], title: "Flashcards")
+        NavigationView {
+            FlashcardSetListView(flashcardSets: [], fetchType: FlashcardSetList.SetType.owned, title: "Flashcards")
+                .environmentObject(User())
+        }
     }
 }
