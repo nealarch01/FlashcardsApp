0a36b95b47606a0dc47e80482d6ed090b06f7f84
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-08-25 13:59:16 -0700

    Create new constructor
    Create constructor that takes a FlashcardSet decodable and returns as FlashcardSet

--- Frontend/iOSApp/iOS-Flashcards-App/iOS-Flashcards-App/Models/FlashcardSet.swift
+++ Frontend/iOSApp/iOS-Flashcards-App/iOS-Flashcards-App/Models/FlashcardSet.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 class FlashcardSet: ObservableObject {
+    static public let titleMax: Int = 32
+    static public let descriptionMax: Int = 300 // Maxes are inclusive
+    
     private(set) var id: UInt64
     private(set) var creator_id: UInt64
     @Published private(set) var title: String
@@ -25,6 +28,26 @@ class FlashcardSet: ObservableObject {
         self.created_at = "Aug 8, 2022"
         self.isPrivate = false
         self.cards = [Flashcard(), Flashcard(), Flashcard(), Flashcard()] // Initialize
+    }
+    
+    // Constructor to convert Decodable FlashcardSet
+    init(decodedSet: FlashcardSetDecodable) {
+        self.id = decodedSet.id
+        self.creator_id = decodedSet.creator_id
+        self.title = decodedSet.title
+        self.description = decodedSet.description
+        self.created_at = decodedSet.created_at
+        self.isPrivate = decodedSet.private
+        self.cards = []
+        if decodedSet.cards == nil {
+            return
+        }
+        
+        // 
+        for card in decodedSet.cards! {
+            self.cards.append(Flashcard(decodedData: card))
+        }
+        
     }
     
     init(id: UInt64, creator_id: UInt64, title: String, description: String, created_at: String, isPrivate: Int, cards: Array<Flashcard>) {
@@ -54,6 +77,10 @@ class FlashcardSet: ObservableObject {
     
 }
 
+
+
+
+
 // Class for decoding API resposne
 class FlashcardSetDecodable: Decodable {
     var id: UInt64
@@ -62,5 +89,32 @@ class FlashcardSetDecodable: Decodable {
     var description: String
     var created_at: String
     var `private`: Bool
-    var cards: Array<FlashcardDecodable>
+    var cards: Array<FlashcardDecodable>?
+}
+
+
+
+
+
+
+
+
+
+class FlashcardSetMetaData: ObservableObject {
+    private(set) var id: UInt64
+    private(set) var creator_id: UInt64
+    @Published private(set) var title: String
+    @Published private(set) var description: String
+    private(set) var created_at: String
+    private(set) var isPrivate: Bool
+    
+    init() {
+        self.id = 0
+        self.creator_id = 0
+        self.creator_id = 0
+        self.title = ""
+        self.description = ""
+        self.created_at = ""
+        self.isPrivate = false
+    }
 }
