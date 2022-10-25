8afe2d09aa869990d6523eb1f5476c10747fcafe
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-10-25 14:11:26 -0700

    Fix URI

--- Backend/src/routes/card-router.ts
+++ Backend/src/routes/card-router.ts
@@ -4,22 +4,22 @@ import { Router } from "express";
 
 class CardRouter {
     // POST ; Creates a new flashcard (does not assign to a flashcard set).
-    createCard = Router().post("/create", CardController.createCard); 
+    createCard = Router().post("/card/create", CardController.createCard); // Passed
 
     // DELETE ; Deletes a flashcard.
-    deleteCard = Router().delete("/delete/:cardID", CardController.deleteCard); 
+    deleteCard = Router().delete("/card/delete/:cardID", CardController.deleteCard); 
 
     // PUT ; Updates the presented text of a flashcard.
-    updatePresentedText = Router().put("/update-presented-text/:cardID", CardController.updatePresentedText);
+    updatePresentedText = Router().put("/card/update-presented-text/:cardID", CardController.updatePresentedText);
 
     // PUT ; Updates the hidden text of a flashcard.
-    updateHiddenText = Router().put("/update-hidden-text/:cardID", CardController.updateHiddenText);
+    updateHiddenText = Router().put("/card/update-hidden-text/:cardID", CardController.updateHiddenText);
 
     // PUT ; Updates both the presented and hidden text of a flashcard.
-    updateCardText = Router().put("/update-card-text/:cardID", CardController.updateCardText);
+    updateCardText = Router().put("/card/update-text/:cardID", CardController.updateCardText);
 
     // GET ; Gets the flashcard with the given cardID.
-    getCard = Router().get("/:cardID", CardController.getCard);
+    getCard = Router().get("card/:cardID", CardController.getCard);
 }
 
 export default new CardRouter();
